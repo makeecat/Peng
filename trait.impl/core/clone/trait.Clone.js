08258ae8a1944aee83161d662ae1b7d25df07249
makeecat <makeecat@users.noreply.github.com>
2024-08-03 19:22:31 +0000

    deploy: 205e6a47372d0e15706cefbe61497fd1b39fa299

---
+++ trait.impl/core/clone/trait.Clone.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"Peng":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.80.0/core/clone/trait.Clone.html\" title=\"trait core::clone::Clone\">Clone</a> for <a class=\"struct\" href=\"Peng/struct.Obstacle.html\" title=\"struct Peng::Obstacle\">Obstacle</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
