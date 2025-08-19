02085ac0d122d50da9202831c705fdb0689598e3
makeecat <makeecat@users.noreply.github.com>
2025-08-19 00:46:00 +0000

    deploy: cbe4b8c89570e038fb7d523b54cb01adbadae640

---
+++ trait.impl/core/convert/trait.From.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["peng_quad",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"enum\" href=\"https://rust-random.github.io/rand/rand_distr/normal/enum.Error.html\" title=\"enum rand_distr::normal::Error\">Error</a>&gt; for <a class=\"enum\" href=\"peng_quad/enum.SimulationError.html\" title=\"enum peng_quad::SimulationError\">SimulationError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;<a class=\"struct\" href=\"https://docs.rs/log/0.4.27/log/struct.SetLoggerError.html\" title=\"struct log::SetLoggerError\">SetLoggerError</a>&gt; for <a class=\"enum\" href=\"peng_quad/enum.SimulationError.html\" title=\"enum peng_quad::SimulationError\">SimulationError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;RecordingStreamError&gt; for <a class=\"enum\" href=\"peng_quad/enum.SimulationError.html\" title=\"enum peng_quad::SimulationError\">SimulationError</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/convert/trait.From.html\" title=\"trait core::convert::From\">From</a>&lt;SpawnError&gt; for <a class=\"enum\" href=\"peng_quad/enum.SimulationError.html\" title=\"enum peng_quad::SimulationError\">SimulationError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[1457]}
