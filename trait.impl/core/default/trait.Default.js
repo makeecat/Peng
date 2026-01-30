c7e0a4dde55423445c27d3754e06c0477ab033aa
makeecat <makeecat@users.noreply.github.com>
2026-01-30 15:16:31 +0000

    deploy: 9ef7187539c103b29967188cb3cd6d84417f81f9

---
+++ trait.impl/core/default/trait.Default.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["peng_quad",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"peng_quad/config/struct.ImuConfig.html\" title=\"struct peng_quad::config::ImuConfig\">ImuConfig</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[300]}
