da77e84499a6b460d3aaae4e07b0d0be3a5712af
makeecat <makeecat@users.noreply.github.com>
2026-02-02 18:38:48 +0000

    deploy: 1859179018d2274ec113c98c2d6e659ac314c962

---
+++ trait.impl/core/error/trait.Error.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["peng_quad",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.93.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"peng_quad/enum.SimulationError.html\" title=\"enum peng_quad::SimulationError\">SimulationError</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[287]}
